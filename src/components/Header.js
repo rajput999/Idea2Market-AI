10d32d863821bf6203ad28f72f896a5aea2f97b1
rajput999 <rajput9997647419@gmail.com>
2024-02-10 00:54:05 +0530

    alt

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () =>{
         <div className={classes.main_div}>
             <div className={classes.left_nav}> 
                 <div className={classes.logo}>
-                <img src="https://i.postimg.cc/qvn5JNK8/logo1.png" className={classes.logo_img}/>
+                <img src="https://i.postimg.cc/qvn5JNK8/logo1.png" className={classes.logo_img} alt="logo"/>
                 </div>
                 <div className={classes.title}>
                     Iterative AI
